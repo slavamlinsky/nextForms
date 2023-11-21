f56a23df1158d6cfb354e98149c448d04b49e2f5
slavamlinsky <slavamlinsky@gmail.com>
2023-11-21 17:18:19 +0200

    add preloader while mutating

--- src/app/_components/create-post.tsx
+++ src/app/_components/create-post.tsx
@@ -4,47 +4,51 @@ import { useRouter } from "next/navigation";
 import { useRef, useState } from "react";
 
 import { api } from "~/trpc/react";
+import Loader from "./loader";
 
 interface CreatePostProps {
-  onSuccess?: ((text: string) => void) | undefined  
+  onSuccess?: ((text: string) => void) | undefined;
 }
 
-
 export const CreatePost: React.FC<CreatePostProps> = ({ onSuccess }) => {
-  const [formShow, setFormShow] = useState(true);  
+  const [formShow, setFormShow] = useState(true);
   const [inputText, setInputText] = useState<string>("");
+
+  const [isLoading, setIsLoading] = useState<boolean>(false);
 
   const showForm = () => {
     setFormShow((prev) => !prev);
   };
 
-  const mutation = api.post.create.useMutation({    
-    onSuccess: () => {    
+  const mutation = api.post.create.useMutation({
+    onSuccess: () => {
       if (onSuccess) {
-        onSuccess(inputText);        
+        onSuccess(inputText);
         // clear input field
         setInputText("");
         // hide form
         setFormShow(false);
       }
     },
-  });  
+  });
 
   const addPost = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
 
     try {
-      await mutation.mutateAsync({name: inputText});
+      setIsLoading(true);
+      await mutation.mutateAsync({ name: inputText });
     } catch (error) {
-      console.error('Mutation failed:', error);
+      console.error("Mutation failed:", error);
+    } finally {
+      setIsLoading(false);
     }
-
     // props.onClick(inputText);
     // setInputText("");
     // setFormShow(false);
   };
 
-  const handleTextChange = (event: React.ChangeEvent<HTMLInputElement>) => {    
+  const handleTextChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     const newText: string = event.target.value;
     setInputText(newText);
   };
@@ -62,25 +66,29 @@ export const CreatePost: React.FC<CreatePostProps> = ({ onSuccess }) => {
 
       {formShow && (
         <div className="flex justify-center">
-          <form
-            className="flex w-1/2 md:w-1/3 flex-col items-center gap-3 rounded-lg bg-orange-300 p-3 text-lg"
-            onSubmit={addPost}
-          >
-            Add new record
-            <input
-              value={inputText} onChange={handleTextChange} 
-              type="text"
-              placeholder="new task"
-              className="w-full px-3 py-2"
-            />
-            <input
-              type="submit"
-              value="add"
-              className="cursor-pointer rounded-md bg-slate-200 px-5 py-1 hover:bg-teal-200"
-            />
-          </form>
+          {isLoading && <Loader />}
+          {!isLoading && (
+            <form
+              className="flex w-2/3 flex-col items-center gap-3 rounded-lg bg-orange-300 p-3 text-lg md:w-1/3"
+              onSubmit={addPost}
+            >
+              Add new record
+              <input
+                value={inputText}
+                onChange={handleTextChange}
+                type="text"
+                placeholder="new task"
+                className="w-full px-3 py-2"
+              />
+              <input
+                type="submit"
+                value="add"
+                className="cursor-pointer rounded-md bg-slate-200 px-5 py-1 hover:bg-teal-200"
+              />
+            </form>
+          )}
         </div>
       )}
     </div>
   );
-}
+};
